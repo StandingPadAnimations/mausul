c94596c17b5456340e7d074a21708c1e57e4199f
Maryam Stellamaris <mahid@standingpad.org>
2026-08-22 02:45:15 -0500

    chore: Add license headers
    Signed-off-by: Maryam Stellamaris <mahid@standingpad.org>

--- src/main.go
+++ src/main.go
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 Maryam Stellamaris
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -10,8 +25,8 @@ import (
 	"net/url"
 	"time"
 
-	"golang.org/x/time/rate"
 	"github.com/coreos/go-systemd/v22/activation"
+	"golang.org/x/time/rate"
 )
 
 type App struct {
